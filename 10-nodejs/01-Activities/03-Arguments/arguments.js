74ddad16a8de6c57497ceec63312d07ddb255a46
Claudio Saillant <claudiojsaillant@gmail.com>
2021-04-14 10:14:40 -0400

    fixing ffolder sstructure

---
+++ 10-nodejs/01-Activities/03-Arguments/arguments.js
@@ -0,0 +1,19 @@
+// process.argv will print out any command line arguments.
+// console.log(process.argv);
+
+const a = {
+    name : "Claudio",
+    last : "Saillant",
+    age : 18,
+    namee : "Claudio",
+    lastt : "Saillant",
+    agee : 18,
+    nameee : "Claudio",
+    lasttt : "Saillant",
+    ageeee : 18,
+    nameeeeee : "Claudio",
+    lasttttttttt : "Saillant",
+    ageeeeeeeeeeee : 18
+}
+
+console.log(a)
